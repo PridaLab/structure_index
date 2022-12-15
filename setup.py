6e3ae6af15b4f366afd27ece5828a8a192aafff2
JulioEI <julioesparzaibanez@gmail.com>
2022-12-15 17:07:18 +0100

    setup file for importing

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='structure_index', 
+      version='0.1', 
+      description = 'Structure Index Method',
+      author = 'Julio Esparza & Enrique R. Sebastian',
+      author_email = 'julioesparzaibanez@gmail.com',
+      packages= find_packages()
+      )
+      
+
